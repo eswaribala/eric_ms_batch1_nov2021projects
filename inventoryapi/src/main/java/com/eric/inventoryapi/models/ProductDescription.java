b7fdd271a00c5ee39bc1c1752518df2df3260f41
eswaribala <parameswaribala@gmail.com>
2021-11-23 10:45:37 +0530

    updated

--- inventoryapi/src/main/java/com/eric/inventoryapi/models/ProductDescription.java
+++ inventoryapi/src/main/java/com/eric/inventoryapi/models/ProductDescription.java
@@ -8,7 +8,10 @@ import javax.persistence.Embeddable;
 import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.format.annotation.DateTimeFormat.ISO;
 
+import lombok.Data;
+
 @Embeddable
+@Data
 public class ProductDescription {
     @Column(name="Product_Name",nullable = false,length = 100)
 	private String productName;
